95e555b49acbe6bb67c2f877cdb1ab4bd4c3343f
mabel-96 <144077864+mabel-96@users.noreply.github.com>
2025-04-02 09:54:34 -0500

    Add files via upload

---
+++ portafolio/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Menú móvil
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    if(menuToggle && menu) {
+        menuTog
